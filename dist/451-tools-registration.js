09dc00da1549141d60832efad7c9ab17e9f49758
Declan Rek <declanrek@gmail.com>
2024-06-24 10:26:33 +0200

    feat: bump 451 tools to version 2, exclude some wordpress paths (#3)

--- dist/451-tools-registration.js
+++ dist/451-tools-registration.js
@@ -2,7 +2,7 @@
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', function () {
       const serviceWorkerUrl = `/451-tools.js?configuration=${encodeURIComponent(
-        '/451-tools-configuration.json?v=176f8fdc86c016f8992c162cbb49bcee'
+        '/451-tools-configuration.json?v=126fd85ce9e8cb01701a72317107a80f'
       )}`;
 
       navigator.serviceWorker
