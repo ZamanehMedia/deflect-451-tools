d8b37ff32a28f2d7e81c3b8d36e158fbca318390
Marcel Oomens <mail@marceloomens.com>
2024-07-04 14:56:17 +0200

    Finalize customizable timeout
    Signed-off-by: Marcel Oomens <mail@marceloomens.com>

--- dist/451-tools-registration.js
+++ dist/451-tools-registration.js
@@ -2,7 +2,7 @@
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', function () {
       const serviceWorkerUrl = `/451-tools.js?configuration=${encodeURIComponent(
-        '/451-tools-configuration.json?v=42f60c595d5232271173703b46047f59'
+        '/451-tools-configuration.json?v=37c690430adf5546d61b2b2700b9845f'
       )}`;
 
       navigator.serviceWorker
