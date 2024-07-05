9f55bff69afa2f9603ad24dfcc91a2c97b27c1fa
Marcel Oomens <marcel@radiozamaneh.com>
2024-07-05 11:14:21 +0200

    Get ready for POC

--- dist/451-tools-registration.js
+++ dist/451-tools-registration.js
@@ -2,7 +2,7 @@
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', function () {
       const serviceWorkerUrl = `/451-tools.js?configuration=${encodeURIComponent(
-        '/451-tools-configuration.json?v=dca54805c7fed57656d94cff48ab32b8'
+        '/451-tools-configuration.json?v=17188f3b30478cfbc4ab67244bd1cad9'
       )}`;
 
       navigator.serviceWorker
