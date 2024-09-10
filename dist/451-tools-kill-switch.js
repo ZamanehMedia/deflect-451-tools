0ac4e7217fe5167c1d1f02ca3948fb0f76d35520
Jamerrone <james.p.perrone@protonmail.com>
2024-09-10 12:48:01 +0200

    feat(kill-switch): remove empty sw and clear cache

--- dist/451-tools-kill-switch.js
+++ dist/451-tools-kill-switch.js
@@ -1,12 +1,14 @@
 (function () {
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-      .getRegistrations()
-      .then((registrations) => {
-        registrations.forEach((registration) => registration.unregister());
-      })
-      .then(() => {
-        navigator.serviceWorker.register('/451-tools-empty-sw.js');
+    navigator.serviceWorker.getRegistrations().then(registrations => {
+      registrations.forEach(registration => {
+        registration.unregister();
       });
+  
+      // Optional: Clear all caches to ensure no old cached resources are causing issues
+      caches.keys().then(cacheNames => {
+        cacheNames.forEach(cacheName => caches.delete(cacheName));
+      });
+    });
   }
 })();
